642933cbe6543918ca3c482208074ce652f82c9d
Rufino Rojas <rojasru@globalhitss.com>
2022-06-07 23:14:02 -0500

    consumiendo api de rick y morty con fetch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 /* import logo from './logo.svg'; */
+import { useState } from 'react';
+
 import image from './assets/rick-morty.png';
 import './App.css';
 
 function App() {
-  const requestApi = () =>{
-    console.log("click....");
+
+  const [personajes, setPersonajes] = useState(null);
+
+  const requestApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setPersonajes(characterApi.results);
   }
+
+  console.log(personajes);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick & Morty</h1>
-        <img src={image} alt='Rick & Morty' className='img-home'/>
+        <img src={image} alt='Rick & Morty' className='img-home' />
         <button onClick={requestApi} className="btn-search">Buscar Personajes</button>
       </header>
     </div>
   );
+
 }
 
 export default App;
