b2f3d11ef038423df54045afb1877f44463a79b2
Rufino Rojas <rojasru@globalhitss.com>
2022-06-08 11:00:34 -0500

    se modifico styles y mostrar personajes

--- src/components/personajes.js
+++ src/components/personajes.js
@@ -4,7 +4,8 @@ export default function Personajes(props) {
     const { personajes, setPersonajes } = props;
 
     const [counter, setCounter] = useState(1);
-    const [res, setRes] = useState([])
+    const [res, setRes] = useState([]);
+    const [bandera, setBandera] = useState(true)
 
     const resetPersonajes = () => {
         setPersonajes(null)
@@ -15,8 +16,8 @@ export default function Personajes(props) {
 
         const requesApi = await fetch(`https://rickandmortyapi.com/api/character?page=${counter + 1}`);
         const response = await requesApi.json();
-
         setRes(response.results);
+        setBandera(false)
     }
 
     return (
@@ -24,47 +25,43 @@ export default function Personajes(props) {
             <h1>Personajes</h1>
             <button className='btn-search' onClick={resetPersonajes}>Volver a Inicio</button>
 
-            <div className='container-characters'>
-                {personajes.map((character, index) => (
-                    <div className='character-container' key={index}>
-                        <div>
-                            <img src={character.image} alt={character.name} />
+            {bandera === true ? (
+                <div className='container-characters'>
+                    {personajes.map((character, index) => (
+                        <div className='character-container' key={index}>
+                            <div>
+                                <img src={character.image} alt={character.name} />
+                            </div>
+                            <div>
+                                <h3>{character.name}</h3>
+                                <h6>
+                                    {
+                                        character.status === "Alive" ? (
+                                            <>
+                                                <span className='alive' />
+                                                Alive
+                                            </>
+                                        ) : (
+                                            <>
+                                                <span className='dead' />
+                                                Dead
+                                            </>
+                                        )
+                                    }
+                                </h6>
+                                <p>
+                                    <span className='text-grey'>Episodios: </span>
+                                    <span>{character.episode.length}</span>
+                                </p>
+                                <p>
+                                    <span className='text-grey'>Especie: </span>
+                                    <span>{character.species}</span>
+                                </p>
+                            </div>
                         </div>
-                        <div>
-                            <h3>{character.name}</h3>
-                            <h6>
-                                {
-                                    character.status === "Alive" ? (
-                                        <>
-                                            <span className='alive' />
-                                            Alive
-                                        </>
-                                    ) : (
-                                        <>
-                                            <span className='dead' />
-                                            Dead
-                                        </>
-                                    )
-                                }
-                            </h6>
-                            <p>
-                                <span className='text-grey'>Episodios: </span>
-                                <span>{character.episode.length}</span>
-                            </p>
-                            <p>
-                                <span className='text-grey'>Especie: </span>
-                                <span>{character.species}</span>
-                            </p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-
-            <button onClick={sigPage} className="btn-search">Siguiente Página</button>
-
-
-            <div className='container-characters'>
+                    ))}
+                </div>
+            ) : (<div className='container-characters'>
                 {res.map((character, index) => (
                     <div className='character-container' key={index}>
                         <div>
@@ -98,8 +95,9 @@ export default function Personajes(props) {
                         </div>
                     </div>
                 ))}
-            </div>
+            </div>)}
 
+            <button onClick={sigPage} className="btn-search">Siguiente Página</button>
         </div>
     )
 
